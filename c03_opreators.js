2d9fe475ee8d4bdbf09af2ea28372dd1f5627a6e
devjinx <t.chareonlertkamol@gmail.com>
2025-01-30 12:32:02 +0700

    c033

--- c03_opreators.js
+++ c03_opreators.js
@@ -1,9 +1,51 @@
-var a = null
-var b  = undefined
-console.log( a == b )
-console.log( a < b )
-console.log( a < b ) || console.log( a == b )
-console.log( a <= b )
-console.log( a > b )
-console.log( a > b ) || console.log( a == b )
-console.log( a >= b )
+/*
+boolean: true, false
+number: 0, non-zero, NaN, Infinity, -Infinity 
+string: "0", "", "non-zero", "non-number"
+array: [], [4], [5,2]
+object: {}, {k: 3}
+undefined
+null
+*/
+plusSign()
+arithmetic()
+logic()
+equality()
+comparison()
+function plusSign() { // +
+    var x = null + undefined //nan
+    console.log(typeof x, x) // at least 5 cases
+    // conclusion: ...
+    console.log(x + x) //nan
+    console.log( null + null ) //0
+    console.log( undefined + undefined ) //nan
+    console.log(( undefined + undefined )+( null + null )) //nan
+}
+function arithmetic() { // -, *, /, %, **
+    console.log(null * undefined) //nan
+    console.log(2 ** 4) //16
+    console.log(13 % 4) //1
+    console.log(8**3/24/3*2) //14.222222222222221
+}
+function logic() { // !, &&, ||
+    console.log(true || true)//true
+    console.log(false || true)//true
+    console.log(true || false)//true
+    console.log(true && 2)//true
+    console.log(!2)//false
+}
+function equality() { // ==, !=, ===, !==
+    console.log("equality")
+    console.log(undefined != null)//false
+    console.log(null == undefined) //true
+    console.log(undefined == true) //false
+    console.log((null === undefined) == (null === undefined)) //true
+    console.log(null !== null) //false
+}
+function comparison() { // <, >, <=, >=
+    console.log(null >= undefined) //false
+    console.log(null > undefined) //false
+    console.log(null <= undefined) //false
+    console.log(false >= true) //false
+    console.log(false <= true) //true
+}
